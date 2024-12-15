a2b39429bd5106a32e788c8763051770d232a775
Kenny Ngu Kai Chun <kennykcngu@gamuda.com.my>
2024-12-15 16:17:26 +0800

    add hover effect for top bar

---
+++ components/FloatingMenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- Hello card here -->
+  <div class="hello-card-container-bot-shadow">
+    <div class="hello-card-container-right-shadow">
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <v-card
+            class="hello-card"
+            v-bind="props"
+            :color="isHovering ? 'primary' : undefined"
+          >
+            <v-card-title class="text-center">
+              <h2 class="semibold" :style="{ color: 'white' }">Hello :)</h2>
+            </v-card-title>
+          </v-card>
+        </template>
+      </v-hover>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.hello-card-container-bot-shadow {
+  position: relative;
+  width: 123px;
+  height: 150px;
+  margin: 30px 30px 30px 0;
+  left: 4vh;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+}
+
+.hello-card-container-right-shadow {
+  width: 150px;
+  height: 123px;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+}
+.hello-card {
+  width: 150px;
+  height: 150px;
+  align-content: center;
+  background: #ef8100;
+  position: relative;
+  clip-path: polygon(
+    0 0,
+    100% 0,
+    100% calc(100% - 27px),
+    calc(100% - 27px) 100%,
+    0 100%
+  );
+}
+
+.hello-card::after {
+  content: '';
+  position: absolute;
+  bottom: 10px;
+  right: 0px;
+  width: 0;
+  height: 0;
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  border-top: 20px solid #ffd700;
+  transform: rotate(135deg);
+  z-index: 2;
+}
+</style>
--- pages/portfolio3.vue
+++ pages/portfolio3.vue
@@ -1,57 +1,83 @@
 <template>
   <div :style="{ position: 'relative' }">
-      <img src="~assets/portfolio3/paintBackground.svg" class="paint-background"></img>
-      <img src="~assets/portfolio3/portrait.png" class="portrait-background"></img>
+    <img
+      src="~assets/portfolio3/paintBackground.svg"
+      class="paint-background"
+    />
+    <img src="~assets/portfolio3/portrait.png" class="portrait-background" />
+
+    <!-- top bar -->
     <div class="top">
       <v-row no-gutters :style="{ height: '100%' }">
+        <!-- signature section -->
         <v-col cols="3"
           ><div :style="{ margin: '12px 0 0 3vw' }">
             <span class="signature">Kenny</span>
-          </div></v-col
-        >
-        <v-col cols="6" class="px-8" :style="{ 'align-content': 'center' }">
-          <div
-            :style="{
-              display: 'flex',
-              'justify-content': 'space-between',
-              width: '100%',
-            }"
-          >
-            <div><span class="top-bar-font">Experience</span></div>
-            <div><span class="top-bar-font">Resume</span></div>
-            <div><span class="top-bar-font">Social media</span></div>
           </div>
         </v-col>
-        <v-col cols="3" :style="{ 'align-content': 'center' }">
+
+        <!-- buttons section -->
+        <v-col cols="6" class="px-8" :style="{ alignContent: 'center' }">
           <div
             :style="{
-              width: '100%',
-              'justify-content': 'center',
               display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              height: '100%',
             }"
           >
-            <v-icon
-              :style="{
-                'align-self': 'center',
-                marginRight: '6px',
-                color: 'var(--text-sec-color)',
-              }"
-              >mdi-phone</v-icon
-            >
-            <span class="top-bar-font">+6013-8035796 </span>
+            <div class="top-buttons">
+              <span class="top-bar-font">Experience</span>
+            </div>
+            <div class="top-buttons">
+              <span class="top-bar-font">Resume</span>
+            </div>
+            <div class="top-buttons">
+              <span class="top-bar-font">Social media</span>
+            </div>
+          </div>
+        </v-col>
+
+        <!-- connect section -->
+        <v-col
+          cols="3"
+          :style="{ padding: 0, margin: 0, justifyItems: 'center' }"
+        >
+          <div
+            class="top-buttons hover-div"
+            :style="{
+              justifyContent: 'center',
+              display: 'grid',
+              height: '100%',
+              alignContent: 'center',
+              background: 'var(--background-orange-color)',
+              width: '50%',
+              cursor: 'pointer',
+            }"
+          >
+            <span class="top-bar-font" :style="{ color: 'white' }">
+              Contact
+            </span>
           </div>
         </v-col>
       </v-row>
     </div>
     <div class="mid">
       <div :style="{ flex: 1, display: 'grid' }">
-        <span class="main-title" :style="{ 'align-content': 'end' }">
+        <span class="main-title" :style="{ alignContent: 'end' }">
           Hey There,
         </span>
         <span class="main-title">I'm Kenny</span>
       </div>
-      <div :style="{ flex: 1, display: 'grid', justifyContent: 'end', marginRight: '14rem' }">
-        <span class="description" :style="{ 'align-content': 'end' }">
+      <div
+        :style="{
+          flex: 1,
+          display: 'grid',
+          justifyContent: 'end',
+          marginRight: '14rem',
+        }"
+      >
+        <span class="description" :style="{ alignContent: 'end' }">
           Currently a fullstack developer.
         </span>
         <span class="description">And I love what i do.</span>
@@ -61,11 +87,15 @@
       <div :style="{ display: 'flex', flex: 1, marginLeft: '6rem' }">
         <span
           class="title"
-          :style="{ 'align-content': 'center', 'margin-right': '12px' }"
-          >2.5</span
-        >
+          :style="{
+            alignContent: 'center',
+            marginRight: '12px',
+            fontSize: '62px',
+          }"
+          >{{ yearOfExperience }}
+        </span>
         <div :style="{ display: 'grid' }">
-          <h2 class="semibold" :style="{ 'align-content': 'end' }">YEARS</h2>
+          <h2 class="semibold" :style="{ alignContent: 'end' }">YEARS</h2>
           <h2 class="semibold">EXPERIENCE</h2>
         </div>
       </div>
@@ -73,11 +103,11 @@
         <div
           :style="{
             display: 'grid',
-            'justify-content': 'end',
-            'margin-right': '16rem',
+            justifyContent: 'end',
+            marginRight: '16rem',
           }"
         >
-          <img :src="gcpLogo" class="gcp-logo"></img>
+          <img :src="gcpLogo" class="gcp-logo" />
           <span class="ace-font">GCP Certified</span>
           <span class="ace-font">Associate</span>
           <span class="ace-font">Cloud Engineer</span>
@@ -87,20 +117,29 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
 import gcpLogo from '@/assets/portfolio3/gcpLogo.svg'
 definePageMeta({
   layout: false,
 })
+
+// 2022 August
+const startDate = new Date(2022, 7)
+const currentDate = new Date()
+const diffInMilliseconds = currentDate.getTime() - startDate.getTime()
+
+// Convert to years and months (Considering leap years)
+const diffInYears = Math.floor(
+  diffInMilliseconds / (1000 * 60 * 60 * 24 * 365.25),
+)
+
+const yearOfExperience = computed(() => {
+  return `${diffInYears}+`
+})
 </script>
 
 <style lang="scss" scoped>
-.top-bar-font {
-  font-size: 1.5rem;
-  color: var(--text-sec-color);
-  text-transform: none;
-}
-
 .top {
   height: 10vh;
 }
@@ -153,6 +192,30 @@ definePageMeta({
   justify-self: center;
 }
 
+// top bar related css
+.top-buttons {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.top-bar-font {
+  font-size: 1.5rem;
+  color: var(--text-sec-color);
+  text-transform: none;
+  cursor: pointer;
+}
+
+.top-bar-font:hover {
+  color: var(--contrast-color);
+}
+
+.hover-div:hover {
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
+}
+
+// for large web screen
 @media (min-width: 2560px) {
   .paint-background {
     top: 60%;
