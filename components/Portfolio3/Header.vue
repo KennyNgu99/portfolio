f0914b54c759fc99dabb81e3797f02c1ef80d29e
kennykcngu <kennykcngu@gamuda.com.my>
2025-01-24 22:29:30 +0800

    update mobile view - header

--- components/Portfolio3/Header.vue
+++ components/Portfolio3/Header.vue
@@ -1,81 +1,75 @@
 <template>
   <div class="top">
-    <v-row no-gutters :style="{ height: '100%' }">
+    <v-row no-gutters :style="{ height: '100%', width: '100%' }">
       <!-- signature section -->
       <v-col cols="3">
-        <div :style="{ margin: '12px 0 0 3vw' }">
-          <span
-            class="signature kenny-font"
-            :style="{ userSelect: 'none', fontSize: '28px' }"
-            @click="$emit('navigate', 'home')"
-          >
+        <div :style="{ height: '100%', display: 'grid', 'justify-items': 'center', 'align-items': 'center' }">
+          <span class="signature kenny-font" @click="$emit('navigate', 'home')">
             Kenny
           </span>
         </div>
       </v-col>
 
       <!-- buttons section -->
-      <v-col cols="6" class="px-8" :style="{ alignContent: 'center' }">
-        <div
-          :style="{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-            height: '100%',
-          }"
-        >
+      <v-col :cols="displayWidth > 600 ? 6 : 7" :class="displayWidth > 600 ? 'px-8' : ''"
+        :style="{ alignContent: 'center' }">
+        <div :style="{
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%',
+          height: '100%',
+        }">
           <div class="top-buttons" @click="$emit('navigate', 'section2Ref')">
-            <div class="top-button-wrapper">
+            <div v-if="displayWidth > 600" class="top-button-wrapper">
               <span class="top-bar-font">Work Experience</span>
             </div>
+            <div v-else><v-icon>mdi-office-building</v-icon></div>
           </div>
           <div class="top-buttons" @click="$emit('navigate', 'projects')">
-            <div class="top-button-wrapper">
+            <div v-if="displayWidth > 600" class="top-button-wrapper">
               <span class="top-bar-font">Projects</span>
             </div>
+            <div v-else><v-icon>mdi-laptop</v-icon></div>
           </div>
           <div class="top-buttons" @click="$emit('navigate', 'funFact')">
-            <div class="top-button-wrapper">
+            <div v-if="displayWidth > 600" class="top-button-wrapper">
               <span class="top-bar-font">Fun Fact</span>
             </div>
+            <div v-else><v-icon>mdi-trophy</v-icon></div>
           </div>
         </div>
       </v-col>
 
       <!-- connect section -->
-      <v-col
-        cols="3"
-        :style="{ padding: 0, margin: 0, justifyItems: 'center' }"
-      >
-        <div
-          class="hover-div"
-          :style="{
-            justifyContent: 'center',
-            display: 'grid',
-            height: '100%',
-            alignContent: 'center',
-            background: 'var(--background-orange-color)',
-            width: '50%',
-            cursor: 'pointer',
-          }"
-          @click="$emit('navigate', 'contact')"
-        >
-          <span
-            :style="{
-              color: 'white',
-              fontSize: '20px',
-              userSelect: 'none',
-            }"
-          >
+      <v-col :cols="displayWidth > 600 ? 3 : 2" :style="{ padding: 0, margin: 0, justifyItems: 'center' }">
+        <div class="hover-div" :style="{
+          justifyContent: 'center',
+          display: 'grid',
+          height: '100%',
+          alignContent: 'center',
+          background: 'var(--background-orange-color)',
+          width: displayWidth > 600 ? '50%' : '100%',
+          cursor: 'pointer',
+        }" @click="$emit('navigate', 'contact')">
+          <span v-if="displayWidth > 600" :style="{
+            color: 'white',
+            fontSize: '20px',
+            userSelect: 'none',
+          }">
             Contact
           </span>
+          <div v-else><v-icon>mdi-account-badge</v-icon></div>
         </div>
       </v-col>
     </v-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const { width: displayWidth } = useDisplay();
+</script>
 
 <style lang="scss" scoped>
 .top {
@@ -98,6 +92,7 @@
   justify-content: center;
   align-items: center;
 }
+
 .top-bar-font {
   font-size: 20px;
   color: var(--text-sec-color);
@@ -126,5 +121,17 @@
 
 .top-button-wrapper:hover .top-bar-font {
   color: var(--contrast-color);
+}
+
+.kenny-font {
+  cursor: pointer;
+  font-size: 28px;
+}
+
+// media queries 
+@media (max-width: 600px) {
+  .kenny-font {
+    font-size: 20px;
+  }
 }
 </style>
